622252a8046259741f82cdc7acbaeb85092550b7
Vivek Kumar <134043577+dev-vivekkr30@users.noreply.github.com>
2025-05-29 16:36:26 +0530

    final-chnages

--- js/datatable.js
+++ js/datatable.js
@@ -7,3 +7,12 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('#quotationPendingTable').DataTable();
 });
+$(document).ready(function () {
+    $('#AllTable').DataTable();
+});
+$(document).ready(function () {
+    $('#upcomingTable').DataTable();
+});
+$(document).ready(function () {
+    $('#ExpiredTable').DataTable();
+});
